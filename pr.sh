7caa031601ec4a69d523132eafd23eb29fd3aeac
alexa <alexa@traiana.com>
2018-12-10 16:25:51 +0200

    pr.sh

---
+++ pr.sh
@@ -0,0 +1,9 @@
+url=`git remote get-url origin`
+url=${url##*:}
+url=${url%.*}
+url="https://github.com/"$url
+
+branch_name=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+pr_url=$url"/compare/"$branch_name"?expand=1"
+open $pr_url
