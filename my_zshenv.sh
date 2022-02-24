8ce3b3a7dd67750a4683c2bee8d7be41936a865e
Alex abugov <abugov@Alexs-MacBook-Pro.local>
2022-02-24 14:09:16 +0200

    my_zshenv.sh

---
+++ my_zshenv.sh
@@ -0,0 +1,12 @@
+# need to install autojump for this: brew install autojump
+#source $HOME/.autojump/etc/profile.d/autojump.sh
+[ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
+
+source $HOME/github/abugov/env/fubectl.source;
+
+#source $HOME/.fzf.bash;
+#source $HOME/.fzf.extras;
+
+#source <(kubectl completion bash | sed 's/kubectl/k/g');
+
+#source <(okroadm completion bash)
